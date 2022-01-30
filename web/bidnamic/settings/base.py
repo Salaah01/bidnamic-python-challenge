3be4bd4f4fdca338890736e7928c24a15ca43096
Salaah Amin <salaah_amin@hotmail.co.uk>
2022-01-30 16:03:27 +0000

    drf config to stop coverting decimals to floats.

--- web/bidnamic/settings/base.py
+++ web/bidnamic/settings/base.py
@@ -137,3 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "COERCE_DECIMAL_TO_STRING": False,
+}
